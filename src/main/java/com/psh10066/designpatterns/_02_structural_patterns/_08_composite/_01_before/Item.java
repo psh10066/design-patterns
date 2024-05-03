a67a6fcb487b276b0fd87ac6d07a3f33923ada75
이호준 <psh10066@gmail.com>
2024-05-04 00:55:38 +0900

    컴포짓 패턴 1부 - 패턴 소개

---
+++ src/main/java/com/psh10066/designpatterns/_02_structural_patterns/_08_composite/_01_before/Item.java
@@ -0,0 +1,12 @@
+package com.psh10066.designpatterns._02_structural_patterns._08_composite._01_before;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Item {
+
+    private String name;
+    private int price;
+}
