f8a5c6c143ea9fb15a008ed3fcdb3549a9fc4b5a
이호준 <psh10066@gmail.com>
2024-05-04 20:16:44 +0900

    프록시 패턴 1부 - 패턴 소개

---
+++ src/main/java/com/psh10066/designpatterns/_02_structural_patterns/_12_proxy/_01_before/Client.java
@@ -0,0 +1,9 @@
+package com.psh10066.designpatterns._02_structural_patterns._12_proxy._01_before;
+
+public class Client {
+
+    public static void main(String[] args) {
+        GameService gameService = new GameService();
+        gameService.startGame();
+    }
+}
