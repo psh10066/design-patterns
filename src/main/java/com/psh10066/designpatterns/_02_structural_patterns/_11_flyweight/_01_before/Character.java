6068f5f049f3c6ff4ba6054930423a8a3eab756f
이호준 <psh10066@gmail.com>
2024-05-04 19:38:57 +0900

    플라이웨이트 패턴 1부 - 패턴 소개

---
+++ src/main/java/com/psh10066/designpatterns/_02_structural_patterns/_11_flyweight/_01_before/Character.java
@@ -0,0 +1,12 @@
+package com.psh10066.designpatterns._02_structural_patterns._11_flyweight._01_before;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class Character {
+
+    private char value;
+    private String color;
+    private String fontFamily;
+    private int fontSize;
+}
