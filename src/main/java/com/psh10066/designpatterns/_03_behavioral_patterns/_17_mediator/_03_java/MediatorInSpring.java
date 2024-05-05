60204602a9c6fab099584e4cde2b929f0298fce1
이호준 <psh10066@gmail.com>
2024-05-05 23:18:57 +0900

    중재자 패턴 4부 - 자바와 스프링에서 찾아보는 패턴

---
+++ src/main/java/com/psh10066/designpatterns/_03_behavioral_patterns/_17_mediator/_03_java/MediatorInSpring.java
@@ -0,0 +1,11 @@
+package com.psh10066.designpatterns._03_behavioral_patterns._17_mediator._03_java;
+
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class MediatorInSpring {
+
+    public static void main(String[] args) {
+        // DispatcherServlet이 HandlerAdapter나 ViewResolver를 모두 가지고 doDispatch에서 사용한다.
+        DispatcherServlet dispatcherServlet;
+    }
+}
