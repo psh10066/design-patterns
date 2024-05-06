205cf9b3d5e314d8d582042bb7972801640da6f4
이호준 <psh10066@gmail.com>
2024-05-06 19:57:37 +0900

    상태 패턴 1부 - 패턴 소개

---
+++ src/main/java/com/psh10066/designpatterns/_03_behavioral_patterns/_20_state/_00_common/Student.java
@@ -0,0 +1,12 @@
+package com.psh10066.designpatterns._03_behavioral_patterns._20_state._00_common;
+
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@RequiredArgsConstructor
+@ToString
+public class Student {
+
+    private final String name;
+}
+
