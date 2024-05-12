f1f5c8f6c855da56bd61e00450d7da697cb4e8d9
이호준 <psh10066@gmail.com>
2024-05-12 21:17:09 +0900

    비지터 패턴 4부 - 자바와 스프링에서 찾아보는 패턴

---
+++ src/main/java/com/psh10066/designpatterns/_03_behavioral_patterns/_23_visitor/_03_java/SearchFileVisitor.java
@@ -0,0 +1,46 @@
+package com.psh10066.designpatterns._03_behavioral_patterns._23_visitor._03_java;
+
+import lombok.AllArgsConstructor;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+
+@AllArgsConstructor
+public class SearchFileVisitor implements FileVisitor<Path> {
+
+    private String fileToSearch;
+    private Path startingDirectory;
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        if (fileToSearch.equals(file.getFileName().toString())) {
+            System.out.println("found " + file.getFileName());
+            return FileVisitResult.TERMINATE;
+        }
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        exc.printStackTrace(System.out);
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+        if (Files.isSameFile(startingDirectory, dir)) {
+            System.out.println("search end");
+            return FileVisitResult.TERMINATE;
+        }
+        return FileVisitResult.CONTINUE;
+    }
+}
