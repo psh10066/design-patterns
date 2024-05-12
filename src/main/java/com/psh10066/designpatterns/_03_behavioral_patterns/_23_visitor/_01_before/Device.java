f87af5c86411116d18cb861b53d04d840813f280
이호준 <psh10066@gmail.com>
2024-05-12 21:16:35 +0900

    비지터 패턴 1부 - 패턴 소개

---
+++ src/main/java/com/psh10066/designpatterns/_03_behavioral_patterns/_23_visitor/_01_before/Device.java
@@ -0,0 +1,4 @@
+package com.psh10066.designpatterns._03_behavioral_patterns._23_visitor._01_before;
+
+public interface Device {
+}
---
+++ src/main/java/com/psh10066/designpatterns/_03_behavioral_patterns/_23_visitor/_01_before/Shape.java
@@ -0,0 +1,6 @@
+package com.psh10066.designpatterns._03_behavioral_patterns._23_visitor._01_before;
+
+public interface Shape {
+
+    void printTo(Device device);
+}
