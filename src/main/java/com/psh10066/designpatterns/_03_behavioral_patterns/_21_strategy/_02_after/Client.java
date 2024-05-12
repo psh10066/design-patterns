4f783435d4c99562ca5c7fcc376c59bdf39fe846
이호준 <psh10066@gmail.com>
2024-05-12 19:46:07 +0900

    전략 패턴 2부 - 패턴 적용하기

---
+++ src/main/java/com/psh10066/designpatterns/_03_behavioral_patterns/_21_strategy/_02_after/Client.java
@@ -0,0 +1,22 @@
+package com.psh10066.designpatterns._03_behavioral_patterns._21_strategy._02_after;
+
+public class Client {
+
+    public static void main(String[] args) {
+        BlueLightRedLight game = new BlueLightRedLight();
+        game.blueLight(new Normal());
+        game.redLight(new Faster());
+
+        game.blueLight(new Speed() {
+            @Override
+            public void blueLight() {
+                System.out.println("blue light");
+            }
+
+            @Override
+            public void redLight() {
+                System.out.println("red light");
+            }
+        });
+    }
+}
---
+++ src/main/java/com/psh10066/designpatterns/_03_behavioral_patterns/_21_strategy/_02_after/Speed.java
@@ -0,0 +1,8 @@
+package com.psh10066.designpatterns._03_behavioral_patterns._21_strategy._02_after;
+
+public interface Speed {
+
+    void blueLight();
+
+    void redLight();
+}
